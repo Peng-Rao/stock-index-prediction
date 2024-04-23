1a1f72142f35f77d7f40ed4cccdacbb004d1de04
Peng Rao <raopend@gmail.com>
2024-04-23 23:27:44 +0800

    predict stage

---
+++ notebook/custom_dataset.py
@@ -0,0 +1,208 @@
+import pandas as pd
+import numpy as np
+import lightning.pytorch as pl
+from pytorch_forecasting import TimeSeriesDataSet
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import os
+from typing import Any
+
+
+class UnivariateSeriesDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        n_lags: int,
+        horizon: int,
+        test_size: float = 0.2,
+        batch_size: int = 16,
+        feature_name: str = "",
+    ) -> None:
+        super().__init__()
+        self.data = data
+        self.n_lags = n_lags
+        self.horizon = horizon
+        self.test_size = test_size
+        self.batch_size = batch_size
+        self.feature_name = feature_name
+        self.training: TimeSeriesDataSet
+        self.validation: TimeSeriesDataSet
+        self.test: TimeSeriesDataSet
+        self.predict: TimeSeriesDataSet
+        self.target_scaler = MinMaxScaler()
+        self.setup()
+
+    def preprocess_data(self):
+        self.data["target"] = self.data[self.feature_name]
+        self.data["time_index"] = np.arange(len(self.data))
+        self.data["group_id"] = 0
+
+    def split_data(self):
+        time_indices = self.data["time_index"].values
+        train_indices, test_indices = train_test_split(
+            time_indices, test_size=self.test_size, shuffle=False
+        )
+        train_indices, val_indices = train_test_split(
+            train_indices, test_size=0.1, shuffle=False
+        )
+        return train_indices, val_indices, test_indices
+
+    def scale_target(self, df, indices):
+        scaled_values = self.target_scaler.transform(df.loc[indices, ["target"]])
+        df.loc[indices, "target"] = scaled_values
+
+    def setup(self, stage=None) -> None:
+        self.preprocess_data()
+        train_indices, val_indices, test_indices = self.split_data()
+
+        train_df = self.data.loc[self.data["time_index"].isin(train_indices)]
+        val_df = self.data.loc[self.data["time_index"].isin(val_indices)]
+        test_df = self.data.loc[self.data["time_index"].isin(test_indices)]
+
+        self.target_scaler.fit(train_df[["target"]])
+        self.scale_target(train_df, train_df.index)
+        self.scale_target(val_df, val_df.index)
+        self.scale_target(test_df, test_df.index)
+
+        self.training = TimeSeriesDataSet(
+            train_df,
+            time_idx="time_index",
+            target="target",
+            group_ids=["group_id"],
+            max_encoder_length=self.n_lags,
+            max_prediction_length=self.horizon,
+            time_varying_unknown_reals=[self.feature_name],
+        )
+        self.validation = TimeSeriesDataSet.from_dataset(
+            self.training, val_df, stop_randomization=True
+        )
+        self.test = TimeSeriesDataSet.from_dataset(
+            self.training, test_df, stop_randomization=True
+        )
+        self.predict = TimeSeriesDataSet.from_dataset(
+            self.training, test_df, stop_randomization=True
+        )
+
+    def train_dataloader(self) -> Any:
+        return self.training.to_dataloader(
+            batch_size=self.batch_size,
+            num_workers=os.cpu_count(),
+            shuffle=False,
+            persistent_workers=True,
+        )
+
+    def val_dataloader(self) -> Any:
+        return self.validation.to_dataloader(
+            batch_size=self.batch_size,
+            num_workers=os.cpu_count(),
+            shuffle=False,
+            persistent_workers=True,
+        )
+
+    def test_dataloader(self) -> Any:
+        return self.test.to_dataloader(
+            batch_size=self.batch_size,
+            num_workers=os.cpu_count(),
+            shuffle=False,
+            persistent_workers=True,
+        )
+
+    def predict_dataloader(self) -> Any:
+        return self.predict.to_dataloader(
+            batch_size=1,
+            num_workers=os.cpu_count(),
+            shuffle=False,
+            persistent_workers=True,
+        )
+    
+class MultivariateSeriesDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        n_lags: int,
+        horizon: int,
+        test_size: float = 0.2,
+        batch_size: int = 16,
+        target_name: str = "Incoming Solar",
+    ):
+        super().__init__()
+        self.data = data
+        self.feature_names = [col for col in data.columns if col != target_name]
+        self.batch_size = batch_size
+        self.test_size = test_size
+        self.n_lags = n_lags
+        self.horizon = horizon
+        self.target_name = target_name
+        self.target_scaler = MinMaxScaler()
+        self.training: TimeSeriesDataSet
+        self.validation: TimeSeriesDataSet
+        self.test: TimeSeriesDataSet
+        self.predict: TimeSeriesDataSet
+        self.setup()
+    
+    def preprocess_data(self):
+        self.data["target"] = self.data[self.target_name]
+        self.data["time_index"] = np.arange(len(self.data))
+        self.data["group_id"] = 0
+
+    def split_data(self):
+        time_indices = self.data["time_index"].values
+        train_indices, test_indices = train_test_split(
+            time_indices, test_size=self.test_size, shuffle=False
+        )
+        train_indices, val_indices = train_test_split(
+            train_indices, test_size=0.1, shuffle=False
+        )
+        return train_indices, val_indices, test_indices
+    
+    def scale_target(self, df, indices):
+        scaled_values = self.target_scaler.transform(df.loc[indices, ["target"]])
+        df.loc[indices, "target"] = scaled_values
+
+    def setup(self, stage=None):
+        self.preprocess_data()
+        train_indices, val_indices, test_indices = self.split_data()
+
+        train_df = self.data.loc[self.data["time_index"].isin(train_indices)]
+        val_df = self.data.loc[self.data["time_index"].isin(val_indices)]
+        test_df = self.data.loc[self.data["time_index"].isin(test_indices)]
+
+        self.target_scaler.fit(train_df[["target"]])
+        self.scale_target(train_df, train_df.index)
+        self.scale_target(val_df, val_df.index)
+        self.scale_target(test_df, test_df.index)
+
+        train_df = train_df.drop(self.target_name, axis=1)
+        val_df = val_df.drop(self.target_name, axis=1)
+        test_df = test_df.drop(self.target_name, axis=1)
+
+        self.training = TimeSeriesDataSet(
+            train_df,
+            time_idx="time_index",
+            target="target",
+            group_ids=["group_id"],
+            max_encoder_length=self.n_lags,
+            max_prediction_length=self.horizon,
+            time_varying_unknown_reals=self.feature_names,
+        )
+        self.validation = TimeSeriesDataSet.from_dataset(
+            self.training, val_df, stop_randomization=True
+        )
+        self.test = TimeSeriesDataSet.from_dataset(
+            self.training, test_df, stop_randomization=True
+        )
+        self.predict = TimeSeriesDataSet.from_dataset(
+            self.training, test_df, stop_randomization=True
+        )
+
+    def train_dataloader(self):
+        return self.training.to_dataloader(batch_size=self.batch_size, shuffle=False)
+
+    def val_dataloader(self):
+        return self.validation.to_dataloader(batch_size=self.batch_size, shuffle=False)
+
+    def test_dataloader(self):
+        return self.test.to_dataloader(batch_size=self.batch_size, shuffle=False)
+
+    def predict_dataloader(self):
+        return self.predict.to_dataloader(batch_size=1, shuffle=False)
