adeb5645b242708ff3e363f66d929a0b813cc51d
Peng Rao <raopend@gmail.com>
2024-04-24 22:15:42 +0800

    Finish lstm univariate baseline

--- notebook/custom_dataset.py
+++ notebook/custom_dataset.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import lightning.pytorch as pl
 from pytorch_forecasting import TimeSeriesDataSet
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 import os
 from typing import Any
@@ -29,7 +28,6 @@ class UnivariateSeriesDataModule(pl.LightningDataModule):
         self.validation: TimeSeriesDataSet
         self.test: TimeSeriesDataSet
         self.predict: TimeSeriesDataSet
-        self.target_scaler = MinMaxScaler()
         self.setup()
 
     def preprocess_data(self):
@@ -47,9 +45,9 @@ class UnivariateSeriesDataModule(pl.LightningDataModule):
         )
         return train_indices, val_indices, test_indices
 
-    def scale_target(self, df, indices):
-        scaled_values = self.target_scaler.transform(df.loc[indices, ["target"]])
-        df.loc[indices, "target"] = scaled_values
+    # def scale_target(self, df, indices):
+    #     scaled_values = self.target_scaler.transform(df.loc[indices, ["target"]])
+    #     df.loc[indices, "target"] = scaled_values
 
     def setup(self, stage=None) -> None:
         self.preprocess_data()
@@ -59,10 +57,10 @@ class UnivariateSeriesDataModule(pl.LightningDataModule):
         val_df = self.data.loc[self.data["time_index"].isin(val_indices)]
         test_df = self.data.loc[self.data["time_index"].isin(test_indices)]
 
-        self.target_scaler.fit(train_df[["target"]])
-        self.scale_target(train_df, train_df.index)
-        self.scale_target(val_df, val_df.index)
-        self.scale_target(test_df, test_df.index)
+        # self.target_scaler.fit(train_df[["target"]])
+        # self.scale_target(train_df, train_df.index)
+        # self.scale_target(val_df, val_df.index)
+        # self.scale_target(test_df, test_df.index)
 
         self.training = TimeSeriesDataSet(
             train_df,
@@ -134,7 +132,7 @@ class MultivariateSeriesDataModule(pl.LightningDataModule):
         self.n_lags = n_lags
         self.horizon = horizon
         self.target_name = target_name
-        self.target_scaler = MinMaxScaler()
+        # self.target_scaler = MinMaxScaler()
         self.training: TimeSeriesDataSet
         self.validation: TimeSeriesDataSet
         self.test: TimeSeriesDataSet
@@ -156,9 +154,9 @@ class MultivariateSeriesDataModule(pl.LightningDataModule):
         )
         return train_indices, val_indices, test_indices
 
-    def scale_target(self, df, indices):
-        scaled_values = self.target_scaler.transform(df.loc[indices, ["target"]])
-        df.loc[indices, "target"] = scaled_values
+    # def scale_target(self, df, indices):
+    #     scaled_values = self.target_scaler.transform(df.loc[indices, ["target"]])
+    #     df.loc[indices, "target"] = scaled_values
 
     def setup(self, stage=None):
         self.preprocess_data()
@@ -168,10 +166,10 @@ class MultivariateSeriesDataModule(pl.LightningDataModule):
         val_df = self.data.loc[self.data["time_index"].isin(val_indices)]
         test_df = self.data.loc[self.data["time_index"].isin(test_indices)]
 
-        self.target_scaler.fit(train_df[["target"]])
-        self.scale_target(train_df, train_df.index)
-        self.scale_target(val_df, val_df.index)
-        self.scale_target(test_df, test_df.index)
+        # self.target_scaler.fit(train_df[["target"]])
+        # self.scale_target(train_df, train_df.index)
+        # self.scale_target(val_df, val_df.index)
+        # self.scale_target(test_df, test_df.index)
 
         train_df = train_df.drop(self.target_name, axis=1)
         val_df = val_df.drop(self.target_name, axis=1)
@@ -196,14 +194,31 @@ class MultivariateSeriesDataModule(pl.LightningDataModule):
             self.training, test_df, stop_randomization=True
         )
 
-    def train_dataloader(self):
-        return self.training.to_dataloader(batch_size=self.batch_size, shuffle=False)
+    def train_dataloader(self) -> Any:
+        return self.training.to_dataloader(
+            batch_size=self.batch_size,
+            num_workers=os.cpu_count(),
+            shuffle=False,
+            persistent_workers=True,
+        )
 
     def val_dataloader(self):
-        return self.validation.to_dataloader(batch_size=self.batch_size, shuffle=False)
+        return self.validation.to_dataloader(
+            batch_size=self.batch_size,
+            num_workers=os.cpu_count(),
+            shuffle=False,
+            persistent_workers=True,
+        )
 
     def test_dataloader(self):
-        return self.test.to_dataloader(batch_size=self.batch_size, shuffle=False)
+        return self.test.to_dataloader(
+            batch_size=self.batch_size,
+            num_workers=os.cpu_count(),
+            shuffle=False,
+            persistent_workers=True,
+        )
 
     def predict_dataloader(self):
-        return self.predict.to_dataloader(batch_size=1, shuffle=False)
+        return self.predict.to_dataloader(
+            batch_size=1, shuffle=False, persistent_workers=True
+        )
--- notebook/modules.py
+++ notebook/modules.py
@@ -57,3 +57,62 @@ class UnivariateLSTM(pl.LightningModule):
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=0.001)
+
+
+class MultivariateLSTM(pl.LightningModule):
+    def __init__(
+        self,
+        input_dim: int = 4,
+        hidden_dim: int = 256,
+        num_layers: int = 2,
+        output_dim: int = 1,
+    ):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.lstm.num_layers, x.size(0), self.hidden_dim).to(
+            self.device
+        )
+        c0 = torch.zeros(self.lstm.num_layers, x.size(0), self.hidden_dim).to(
+            self.device
+        )
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_pred = self(x["encoder_cont"])
+        y_pred = y_pred.squeeze(1)
+        loss = F.mse_loss(y_pred, y[0])
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_pred = self(x["encoder_cont"])
+
+        y_pred = y_pred.squeeze(1)
+
+        loss = F.mse_loss(y_pred, y[0])
+        self.log("val_loss", loss)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_pred = self(x["encoder_cont"])
+        y_pred = y_pred.squeeze(1)
+        loss = F.mse_loss(y_pred, y[0])
+        self.log("test_loss", loss)
+
+    def predict_step(self, batch, batch_idx, dataloader_idx=0):
+        x, y = batch
+        y_pred = self(x["encoder_cont"])
+        y_pred = y_pred.squeeze(1)
+        return y_pred
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
